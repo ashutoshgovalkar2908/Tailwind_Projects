c44ccce867280955214c03f9cbf66ca296d88f3a
ashutoshgovalkar2908 <ashutoshgovalkar942@gmail.com>
2024-05-07 14:39:26 +0530

    write js for down arrow applying transform.

--- outputFiles/script.js
+++ outputFiles/script.js
@@ -57,41 +57,72 @@ function toggleFavouriteOut(){
 // function for footer menu open and hide
 
 document.querySelector("#openShop").addEventListener("click",openShop);
+let openshop = document.getElementById("openShop");
+
 document.querySelector("#sell").addEventListener("click",openSell);
+let opensell = document.getElementById("sell");
+
 document.querySelector("#about").addEventListener("click",openAbout);
+let openabout = document.getElementById("about");
+
 document.querySelector("#help").addEventListener("click",openHelp);
+let openhelp = document.getElementById("help");
 
 function openShop() {
     let shopOption = document.getElementById("shop-option");
+    let downarrow = document.getElementById("down-arrow1");
+
     if (shopOption.style.display === "none" || shopOption.style.display === "") {
         shopOption.style.display = "block";
+        openshop.style.backgroundColor = "#4c59b9";
+        downarrow.style.transform = 'rotate(180deg)';
     } else {
         shopOption.style.display = "none";
+        openshop.style.backgroundColor = "#3140af";
+        downarrow.style.transform = 'rotate(360deg)';
     }
 }
 function openSell(){
     let sellOption = document.getElementById("sell-option");
+    let downarrow = document.getElementById("down-arrow2");
+
     if (sellOption.style.display === "none" || sellOption.style.display === "") {
         sellOption.style.display = "block";
+        opensell.style.backgroundColor ="#4c59b9";
+        downarrow.style.transform = 'rotate(180deg)';
     } else {
         sellOption.style.display = "none";
+        opensell.style.backgroundColor ="#3140af";
+        downarrow.style.transform = 'rotate(360deg)';
     }
 }
 
 function openAbout(){
     let aboutOption = document.getElementById("about-option");
+    let downarrow = document.getElementById("down-arrow3");
+
     if (aboutOption.style.display === "none" || aboutOption.style.display === "") {
         aboutOption.style.display = "block";
+        openabout.style.backgroundColor = "#4c59b9";
+        downarrow.style.transform = 'rotate(180deg)';
     } else {
         aboutOption.style.display = "none";
+        openabout.style.backgroundColor = "#3140af";
+        downarrow.style.transform = 'rotate(360deg)';
     }
 }
 function openHelp(){
     let helpOption = document.getElementById("help-privacy");
+    let downarrow = document.getElementById("down-arrow4");
+
     if (helpOption.style.display === "none" || helpOption.style.display === "") {
         helpOption.style.display = "block";
+        openhelp.style.backgroundColor ="#4c59b9";
+        downarrow.style.transform = 'rotate(180deg)';
     } else {
         helpOption.style.display = "none";
+        openhelp.style.backgroundColor ="#3140af";
+        downarrow.style.transform = 'rotate(360deg)';
     }
 }
 
