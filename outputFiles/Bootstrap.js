ca2853b33f7539dcf3b55cfccc572268c66ea270
ashutoshgovalkar2908 <ashutoshgovalkar942@gmail.com>
2024-06-26 12:31:51 +0530

    make changes in components ,utility API section2.

--- outputFiles/Bootstrap.js
+++ outputFiles/Bootstrap.js
@@ -49,3 +49,48 @@ function toggleNav(){
         navSmall.style.display = "block";
     }
 }
+
+let customizeBtn1 = document.querySelector("#cstmBtn1");
+let customizeBtn2 = document.querySelector("#cstmBtn2");
+let customizeBtn3 = document.querySelector("#cstmBtn3");
+
+customizeBtn2.addEventListener("click", roundedBorder1);
+customizeBtn1.addEventListener("click", roundedBorder2);
+customizeBtn3.addEventListener("click", roundedBorder3);
+
+function roundedBorder1(){
+    customizeBtn2.style.backgroundColor = "#0d6efd";
+    customizeBtn2.style.borderRadius = "6px";
+    customizeBtn2.style.color = "#fff";
+    customizeBtn1.style.backgroundColor = "";
+    customizeBtn1.style.color = "#6ea8fe";
+    customizeBtn3.style.backgroundColor = "";
+    customizeBtn3.style.color = "#6ea8fe";
+}
+function roundedBorder2(){
+    customizeBtn1.style.backgroundColor = "#0d6efd";
+    customizeBtn1.style.borderRadius = "6px";
+    customizeBtn1.style.color = "#fff";
+    customizeBtn2.style.backgroundColor = "";
+    customizeBtn2.style.color = "#6ea8fe";
+    customizeBtn3.style.backgroundColor = "";
+    customizeBtn3.style.color = "#6ea8fe";
+}
+function roundedBorder3(){
+    customizeBtn3.style.backgroundColor = "#0d6efd";
+    customizeBtn3.style.borderRadius = "6px";
+    customizeBtn3.style.color = "#fff";
+    customizeBtn1.style.backgroundColor = "";
+    customizeBtn1.style.color = "#6ea8fe";
+    customizeBtn2.style.backgroundColor = "";
+    customizeBtn2.style.color = "#6ea8fe";
+}
+
+let customizeBtn4 = document.querySelector("#cstmBtn4");
+customizeBtn4.addEventListener("click", rounded);
+
+function rounded(){
+    customizeBtn4.style.color = "#6ea8fe";
+    customizeBtn4.style.backgroundColor = "#fff";
+    customizeBtn4.style.border = "2px solid #fff";
+}
